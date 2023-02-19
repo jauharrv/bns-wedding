13208bb0035ae6b73b36ff8d93d309c8e977543f
Jauhar Ravi <jauharravi23@gmail.com>
2023-02-19 19:35:50 +0700

    updates

--- src/components/Section4.js
+++ src/components/Section4.js
@@ -5,21 +5,21 @@ import Text from './Text';
 
 const Section4 = () => {
   return (
-    <section className="relative bg-purple-50 -mt-20 px-6 overflow-hidden" id="event">
-      <div className="pt-24 bg-purple-50">
+    <section className="relative bg-red-800 -mt-20 px-6 overflow-hidden" id="event">
+      <div className="pt-24 bg-red-800">
         <div className="flex justify-center" data-aos="zoom-in-up">
           <Graphic1 />
         </div>
 
-        <div className="text-sm text-[#b692ca] text-center relative z-20" data-aos="zoom-in-up">
+        <div className="text-sm text-[#f59e0b] text-center relative z-20" data-aos="zoom-in-up">
           <p className="mt-8">
             Kehadiran serta doa restu Bapak/Ibu/Saudara/i merupakan suatu kebahagiaan dan kehormatan
             bagi kami.
           </p>
-          <p className="font-semibold text-purple-900 mt-2">{`Wassalamu'alaikum Warrahmatullahi Wabarrakatuh`}</p>
+          <p className="font-semibold text-amber-500 mt-2">{`Wassalamu'alaikum Warrahmatullahi Wabarrakatuh`}</p>
         </div>
 
-        <div className="text-center font-semibold text-[#b692ca] mt-8 relative z-10">
+        <div className="text-center font-semibold text-[#f59e0b] mt-8 relative z-10">
           <p data-aos="fade-down">Minggu</p>
           <div className="flex justify-center items-center space-x-2 mt-2">
             <p className="font-bold text-5xl" data-aos="fade-right">
@@ -32,13 +32,13 @@ const Section4 = () => {
           </div>
         </div>
 
-        <div className="mt-8 text-center text-purple-900 relative z-10" data-aos="fade-right">
+        <div className="mt-8 text-center text-amber-500 relative z-10" data-aos="fade-right">
           <Text className="text-2xl ">Akad Nikah</Text>
           <div className="mt-4 text-lg">
             <p>08.00 WIB</p>
             </div>
         </div>
-        <div className="mt-8 text-center text-purple-900 relative z-10" data-aos="fade-left">
+        <div className="mt-8 text-center text-amber-500 relative z-10" data-aos="fade-left">
           <Text className="text-2xl ">Resepsi</Text>
           <div className="mt-4 text-lg">
             <p>10.00 WIB</p>

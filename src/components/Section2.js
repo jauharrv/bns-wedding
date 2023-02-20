973a2bdca5ae2d14be4b33402ee12cfbe47fce94
Jauhar Ravi <jauharravi23@gmail.com>
2023-02-20 13:46:45 +0700

    updates

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -6,16 +6,19 @@ const Section2 = () => {
     <section
       className="text-center text-[#f59e0b] relative z-10 p-6"
       style={{
-        backgroundImage: `url(/images/bg1.png)`,
-        backgroundSize: '220%',
+        // backgroundImage: `url(/images/bg1.png)`,
+        // backgroundSize: '220%',
         // backgroundRepeat: 'round',
-        backgroundClip: 'border-box',
-        backgroundClip: 'border-box',
+        // backgroundClip: 'border-box',
+        // backgroundClip: 'border-box',
         backgroundColor: '#991b1b',
       }}
     >
       <div data-aos="zoom-in-up">
         <img src="/images/bismillah.png" alt="Bismillah" className="h-12 mx-auto object-cover" />
+        <p style={{ fontFamily: 'B Davat' }} className="mt-10 text-lg">
+          ِبِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيْم
+        </p>
         <p style={{ fontFamily: 'B Davat' }} className="mt-4 text-lg">
           وَمِنْ آيَاتِهِ أَنْ خَلَقَ لَكُمْ مِنْ أَنْفُسِكُمْ أَزْوَاجًا لِتَسْكُنُوا إِلَيْهَا
           وَجَعَلَ بَيْنَكُمْ مَوَدَّةً وَرَحْمَةً ۚ إِنَّ فِي ذَٰلِكَ لَآيَاتٍ لِقَوْمٍ
